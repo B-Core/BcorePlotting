3002db47d88731e5564b939751ddfdc2f33e1ecb
Wes <hortowe@ohsu.edu>
2017-03-06 15:27:21 -0800

    Roxygenized MultipletestingCorrection.R. Could use review by someone who is more familiar with these functions.

--- R/MultipleTestingCorrection.R
+++ R/MultipleTestingCorrection.R
@@ -17,39 +17,33 @@ function(norm_x, p_mat, q_list, metadata, plotdata, plot2file = TRUE,
                        bonusMDS = FALSE, oneclass = NULL, qcut = 0.1, 
                        histbins=40, colorspec = c("#7b3294", "#008837"),
                        facSel = NULL, filesep="/") {
-
-# Wrapper for qcQvalue function to verify q-value analysis terms
-# Plots for each design factor (default) or for factors specified in facSel
-#   1) Histogram of p values that were included in the design
-#   2) qvalue's default plots, with full qvalue range c(0,1) plotted
-#      Look for even descent to pi0 in the top left tuning plot
-#      The slow/steep rise in q-values in remaining plots depends on resolving power of data
-#   3) Optional MDS plot (bonusMDS=TRUE) at q-value cut (default 0.1, see below)
-#
-# Arguments
-#   norm_x: abundance data input into regression (eg, regressMatrix()). nrow(norm_x)==length(p_mat)
-#   p_mat: matrix of p-values for each factor. Returned by regressMatrix()
-#   q_list: S3 object; list of p-value lists for each factor. Returned by regressMatrix() #Feedback supposed to be q-value lists?
-#   metadata: list of factors; each list element is a factor (eg, sex = c("m", "f"...)) #Feedback perhaps as.factor(c("m","f"))?
-#             Vector order same as norm_x column order
-#             was attribs
-#   plotdata is a list of info relevant to labeling and saving the plot #Feedback with the following elements:
-#     plotdir:  plot destination directory
-#     plotbase:  base filename for the plot. Suggest: bias reduction method
-#     plottitle:  title for all plots
-#     plotSubtitle:  optional subtitle; suggest lm_expression
-#   plot2file if TRUE, otherwise to studio...
-#   bonusMDS: if TRUE plots an MDS of data at qcut (see below)
-#   oneclass: metadata factor for labeling bonusMDS data; #Feedback is this actually a factor, or is it a vector?
-#             if NULL or not specified, defaults to the first element of metadata
-#   qcut: either a number between 0 and 1 used as an upper qvalue limit for MDS,
-#     OR a number >1, assumed to be the top n qvalues to use in MDS
-#   histbins: # bins for p-value histogram; may want to change if there is suspicious behavior
-#   colorspec: optional range of colors for MDS plot. Defaults to hotpink/green!
-#   facSel: optional vector of experimental factors to plot (rather than all factors) #Feedback to plot for the MDS plot? What if you want to plot based on second element of metadata list?
-#   #Feedback what about filesep?
-# Returns... Error message or... #Feeback LOL! Thank you for the good humor!!
-#
+  #' Wrapper for qcQvalue function to verify q-value analysis terms
+  #' @description Creates plots for each design factor (default behavior), or for factors facSel
+  #' 1) Histogram of p-values that were included in the design
+  #' 2) qvalue's default plots with full q-value range (c(0,1)).
+  #' 3) Optional MDS plot at q-value cut
+  #' @param norm_x abundance data input into regression (eg, regressMatrix()). nrow(norm_x)==length(p_mat)
+  #' @param p_mat matrix of p-values for each factor. Returned by regressMatrix()
+  #' @param q_list S3 object; list of p-value lists for each factor. Returned by regressMatrix() 
+  #' @param metadata list of factors; each list element is a factor (eg, sex = c("m", "f"...)) #Feedback perhaps as.factor(c("m","f"))?
+  #'             Vector order same as norm_x column order
+  #'             was attribs previously
+  #' @param plotdata is a list of info relevant to labeling and saving the plot With the following elements:
+  #'     plotdir:  plot destination directory
+  #'     plotbase:  base filename for the plot. Suggest: bias reduction method
+  #'     plottitle:  title for all plots
+  #'     plotSubtitle:  optional subtitle; suggest lm_expression
+  #' @param plot2file logical. if TRUE, write files using plotdata, otherwise to studio graphics viewer
+  #' @param bonusMDS logical. if TRUE plots an MDS of data at qcut (see below). Default is FALSE (no plot)
+  #' @param oneclass metadata vector for labeling bonusMDS data; if NULL or not specified, defaults to the first element of metadata
+  #' @param qcut either a number between 0 and 1 used as an upper qvalue limit for MDS, OR a number >1, assumed to be the top n qvalues to use in MDS
+  #' @param histbins number of bins for p-value histogram; may want to change if there is suspicious behavior
+  #' @param colorspec optional range of colors for MDS plot. Defaults to hotpink/green!
+  #' @param facSel optional vector of experimental factors to plot (rather than all factors) #Feedback to plot for the MDS plot? What if you want to plot based on second element of metadata list?
+  #' @param filesep string that adds to end of directory path.
+  #' @return Writes plots to files or to Rstudio viewer. Notes - Looking for even descent to pi0 in top left tuning plot. The slow/steep rise in q-values
+  #' in remaining plots depends on the resolving power of the data.
+  #' @export
   
   # Define factors to plot
   if(is.null(facSel)) {
@@ -116,38 +110,38 @@ function (norm_x, pvalue_v, obj_qvalue, attribs, oneclass, plotdata,
                       colorspec, qcut=1, histbins=40, plot2file = FALSE, 
                       p_hist=TRUE, q_plots=TRUE, MDS_plot=FALSE, 
                       filesep='/') {
-# This is a check on the proper running of q-value analysis
-# plots:
-#  1) histogram of p-values input to q-value. 
-#     these must NOT have a right peak or qvalue() will return spurious results
-#  2) qvalue's default plots, with full qvalue range c(0,1) plotted
-#     these should show an even descent to pi0 in the top left tuning plot,
-#     and a slow or steep rise in q-values in remaining plots depending on 
-#     resolving power of data
-#  3) MDS plot restricted by q-value cut.
-#  norm_x:  abundance data, vector or matrix. nrow(norm_x)==length(pvalue_v) 
-#  pvalue_v: vector of p-values previously used as input to qvalue()
-#  obj_qvalue: S3 object of qvalue class (a list!) returned by qvalue()
-#  qcut: either a number between 0 and 1 used as an upper qvalue limit for MDS,
-#     OR a number >1, assumed to be the top n qvalues to use in MDS
-#  attribs:  list of sample classifications to be tracked in clustering
-#     each list element contains a string vector with one label per sample
-#  oneclass: string name of attribs element to be used in MDS plot
-#  p_hist, q_plots, MDS_plot: flags to plot (TRUE) or skip (FALSE) plot types
-#     MDS_plot now defaults to FALSE -- please use plotRatios() instead
-#     after selecting a q-value cut based on these QC plots
-#  plotdata is a list of info relevant to labeling and saving the plot
-#     plotdir:  plot destination directory
-#     plotbase:  base filename for the plot. Suggest: bias reduction method
-#     plottitle:  title for all plots
-#     plotSubtitle:  optional subtitle; suggest lm_expression
-#  colorspec is a vector of color specifiers for colorRampPalette  
-#  histbins: number of bins for p-value histogram
-#  plot2file: if TRUE save plots to location defined in plotdata$plotdir
-#  fac_sel:  NULL defaults to plotting all factors; can specify subset of factors in a vector
-#  lm_expression is the linear model (eg, "y ~ factor1 + factor2"), included as a subtitle and filename value
-#  p_hist, q_plots, MDS_plot FALSE allow suppression of a plot type
-#  filesep: filepath separator
+  #' Checks proper running of q-value analysis
+  #' @description Creates plots for each design factor (default behavior), or for factors facSel
+  #' 1) Histogram of p-values that were included in the design. Must not have right peak.
+  #' 2) qvalue's default plots with full q-value range (c(0,1)).
+  #' 3) Optional MDS plot at q-value cut
+  #' @param norm_x abundance data input into regression (eg, regressMatrix()). nrow(norm_x)==length(p_mat)
+  #' @param pvalue_v vector of p-values previously used as input to qvalue()
+  #' @param obj_qvalue: S3 object of qvalue class (a list!) returned by qvalue()
+  #' @param attribs  list of sample classifications to be tracked in clustering
+  #'     each list element contains a string vector with one label per sample
+  #' @param oneclass metadata vector for labeling bonusMDS data; if NULL or not specified, defaults to the first element of metadata
+  #' @param plotdata is a list of info relevant to labeling and saving the plot With the following elements:
+  #'     plotdir:  plot destination directory
+  #'     plotbase:  base filename for the plot. Suggest: bias reduction method
+  #'     plottitle:  title for all plots
+  #'     plotSubtitle:  optional subtitle; suggest lm_expression
+  #' @param colorspec optional range of colors for MDS plot. Defaults to hotpink/green!
+  #' @param qcut either a number between 0 and 1 used as an upper qvalue limit for MDS,
+  #'     OR a number >1, assumed to be the top n qvalues to use in MDS
+  #' @param histbins number of bins for p-value histogram; may want to change if there is suspicious behavior
+  #' @param plot2file logical. if TRUE, write files using plotdata, otherwise to studio graphics viewer
+  #' @param p_hist logical. TRUE (default) to plot this type. 
+  #' @param q_plots logical. TRUE (default) to plot this type.
+  #' @param MDS_plot logical. TRUE to plot this type. FALSE is default. Will plot after selecting q-value cut based on QC plots. Use plotRatios() instead!
+  #' @param filesep string that adds to end of directory path.
+  #' @return Writes plots to files or to Rstudio viewer. Notes - Looking for even descent to pi0 in top left tuning plot. The slow/steep rise in q-values
+  #' in remaining plots depends on the resolving power of the data.
+
+  # Not sure where these are coming from...
+  #  fac_sel:  NULL defaults to plotting all factors; can specify subset of factors in a vector
+  #  lm_expression is the linear model (eg, "y ~ factor1 + factor2"), included as a subtitle and filename value
+
 
   # constants
   fudgefac = 2 # fold excess permitted in rows recovered by top n qcut when specified as a quantile of qvalues
@@ -248,28 +242,28 @@ qc.clusters <-
 function (normmat, rawmat, attribs, oneclass, plotdata,
                         colorspec, center = 'norm', clim.pct, mask = NULL, 
                         plot2file = FALSE, filesep='/') {
-# This function considers only a single clustering variable... Will need to be gneralized
-# Data will be centered around the average of all data in the normalized data set
-# As a QC plot, want to filter out non-changing data, otherwise the alg gets slow
-# Uses the attributes to cluster the data; could make this a longer list...
-#  normmat:  data matrix 
-#  rawmat:  raw data matrix (optionally used to center normmat)
-#  attribs:  list of sample classifications to be tracked in clustering
-  # each list element contains a string vector with one label per sample
-#  oneclass: string name of attribs element to be used in MDS plot
-#  plotdata is a list of info relevant to labeling and saving the plot
-#    plotdir:  plot destination directory
-#    plotbase:  base filename for the plot
-#    plottitle:  title for all plots
-#  colorspec is a vector of color specifiers for colorRampPalette  
-#  center:  center data on 'norm' for normalized average
-#           none indicates no-centering needed for zero-centered data
-#           any other value will trigger centering on the average of rawmat
-#  clim.pct:  0:1 - fraction of data to limit max color
-#  mask:  NULL default masks rows with SD > SD of entire data set; 
-#         Numeric vector of length 1 masks rows with SD > value
-#         Vector of length nrows(normmat) is a custom mask   ***** TBImplemented
-#  plot2file:  if true, plot to file as specified in plotdata
+  #' Creates a heatmap and an MDS plot
+  #' @description This function currently only considers a single clustering variable and will need to be generalized.
+  #' Data will be centered around the average of all data in the normalized data set
+  #' As a QC plot, want to filter out non-changing data, otherwise it takes a LONG time to complete
+  #' @param normmat data matrix
+  #' @param rawmat  raw data matrix (optionally used to center normmat)
+  #' @param attribs  list of sample classifications to be tracked in clustering 
+  #' each list element contains a string vector with one label per sample
+  #' @param oneclass string name of attribs element to be used in MDS plot
+  #' @param plotdata is a list of info relevant to labeling and saving the plot
+  #'    plotdir:  plot destination directory
+  #'    plotbase:  base filename for the plot
+  #'    plottitle:  title for all plots
+  #' @param  colorspec is a vector of color specifiers for colorRampPalette  
+  #' @param  center  center data on 'norm' for normalized average
+  #'           none indicates no-centering needed for zero-centered data
+  #'           any other value will trigger centering on the average of rawmat
+  #' @param  clim.pct  0:1 - fraction of data to limit max color
+  #' @param  mask  NULL default masks rows with SD > SD of entire data set; 
+  #'         Numeric vector of length 1 masks rows with SD > value
+  #'         Vector of length nrows(normmat) is a custom mask   ***** TBImplemented
+  #' @param  plot2file  if true, plot to file as specified in plotdata
 
   # imports
   require(NMF)
